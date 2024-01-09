761db7373b1ab02b810b15aa1648a07fd28f6693
DAPR <101226963+DanielPalomino28@users.noreply.github.com>
2024-01-09 14:37:26 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ThirdPerson.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
